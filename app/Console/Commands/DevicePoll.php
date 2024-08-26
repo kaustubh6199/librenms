2ceaa256fcaac81a66adced817d83fb8f8bc17d3
Steven Wilton <swilton@fluentit.com.au>
2024-08-26 14:01:07 +0800

    phpstan fixes

--- app/Console/Commands/DevicePoll.php
+++ app/Console/Commands/DevicePoll.php
@@ -8,6 +8,7 @@ use App\Jobs\PollDevice;
 use App\Models\Device;
 use App\Polling\Measure\MeasurementManager;
 use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Log;
 use LibreNMS\Config;
@@ -41,7 +42,8 @@ class DevicePoll extends LnmsCommand
         if ($this->argument('device spec') == '-') {
             $stdin = fopen('php://stdin', 'r');
             $job_args = [];
-            array_walk(array_filter($this->options()), function ($val, $opt) use (&$job_args) {
+	    $temp_ags = array_filter($this->options());
+            array_walk(array_filter($temp_args, function ($val, $opt) use (&$job_args) {
                 if ($opt !== 'verbose') {
                     $job_args["--$opt"] = $val;
                 }
